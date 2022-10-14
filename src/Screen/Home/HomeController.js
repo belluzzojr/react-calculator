3a3cd8c0cfc465769d85f307008eedc5d159a34a
belluzzojr <belluzzojrb@gmail.com>
2022-10-14 14:05:52 -0300

    React Calculator

--- src/Screen/Home/HomeController.js
+++ src/Screen/Home/HomeController.js
@@ -3,35 +3,81 @@ import HomeView from './HomeView';
 
 const HomeController = () => {
 
-    const [number, setNumber] = useState(0)
+    const [number, setNumber] = useState(0);
     const [number2, setNumber2] = useState(0);
-    const [operator, setOperator] = useState("+")
+    const [operator, setOperator] = useState("#");
+
     const [results, setResults] = useState(0);
 
     const buttonCalc = (e) => {
-        var input = e.target.value
+
+    var input = e.target.value
+
         if (number === 0){
             setNumber(input);
-        } else 
-            setNumber(number + input);
+        }
+
+        if (number2 === 0 && operator === '+'){
+            setNumber2(input);
         } 
+
+        if (number2 === 0 && operator === '-'){
+            setNumber2(input);
+        } 
+
+        if (number2 === 0 && operator === 'x'){
+            setNumber2(input);
+        } 
+
+    } 
+
+    const buttonSum = (e) => {
+        var inputOperator = e.target.value
+
+        if (operator === "#"){
+            setOperator(inputOperator);
+        } 
+    }
 
     const buttonClear = () => {
         setNumber(0);
+        setNumber2(0);
+        setOperator("#");
+        setResults(0);
     }
+
     const buttonDev = () => {
         alert("System in development");
     }
 
+    const buttonTotals = () => {
+
+            if (operator === "+"){
+                setResults(parseInt(number) + parseInt(number2));
+            }
+
+            if (operator === "-"){
+                setResults(parseInt(number) - parseInt(number2));
+            }
+
+            if (operator === "x"){
+                setResults(parseFloat(number) * parseFloat(number2));
+            }
+    }
+
     return (
         <HomeView
+
         number={number}
         number2={number2}
         operator={operator}
         results={results}
+
         buttonCalc={buttonCalc}
         buttonDev={buttonDev}
         buttonClear={buttonClear}
+        buttonSum={buttonSum}
+        buttonTotals={buttonTotals}
         />
     )
 }
--- src/Screen/Home/HomeView.js
+++ src/Screen/Home/HomeView.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import "./Home.css"
 
-export default function HomeView ( { buttonDev, buttonCalc, buttonClear, number, number2, operator, results} ){
+export default function HomeView ( { buttonDev, buttonCalc, buttonClear, 
+    number, number2, operator, results , 
+    buttonSum, buttonTotals } ){
 
     return(
         <div className='home'>
@@ -17,32 +19,32 @@ export default function HomeView ( { buttonDev, buttonCalc, buttonClear, number,
         <div className='step2'>
         <div className='buttonsline'>
             <button className='buttonfirst' onClick={buttonClear}>C</button>
-            <button className='buttonmid'>%</button>
-            <button className='buttonmid'>X</button>
-            <button className='buttonend'>!</button>
+            <button className='buttonmid' onClick={buttonDev}>%</button>
+            <button className='buttonmid' onClick={buttonSum} value={"x"}>X</button>
+            <button className='buttonend' onClick={buttonDev}>!</button>
         </div>
         <div className='buttonsline'>
             <button className='buttonnumbers' onClick={buttonCalc} value={1}>1</button>
             <button className='buttonnumbers' onClick={buttonCalc} value={2}>2</button>
             <button className='buttonnumbers' onClick={buttonCalc} value={3}>3</button>
-            <button className='buttonend2'>-</button>
+            <button className='buttonend2' onClick={buttonSum} value={"-"}>-</button>
         </div>
         <div className='buttonsline'>
             <button className='buttonnumbers' onClick={buttonCalc} value={4}>4</button>
             <button className='buttonnumbers' onClick={buttonCalc} value={5}>5</button>
             <button className='buttonnumbers' onClick={buttonCalc} value={6}>6</button>
-            <button className='buttonend2' onClick>+</button>
+            <button className='buttonend2' onClick={buttonSum} value={"+"}>+</button>
         </div>
         <div className='buttonsline'>
             <button className='buttonnumbers' onClick={buttonCalc} value={7}>7</button>
             <button className='buttonnumbers' onClick={buttonCalc} value={8}>8</button>
             <button className='buttonnumbers' onClick={buttonCalc} value={9}>9</button>
-            <button className='buttonend2'>=</button>
+            <button className='buttonend2' onClick={buttonTotals}>=</button>
         </div>
         <div className='buttonsline'>
-            <button className='buttonnumbers'>%</button>
-            <button className='buttonnumbers'>0</button>
-            <button className='buttonnumbers'>.</button>
+            <button className='buttonnumbers' onClick={buttonDev}>%</button>
+            <button className='buttonnumbers' onClick={buttonCalc} value={0}>0</button>
+            <button className='buttonnumbers' onClick={buttonDev}>.</button>
             <button className='buttonend2' onClick={buttonDev}>?</button>
         </div>
     </div>
